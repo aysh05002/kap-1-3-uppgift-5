50f9edd336556801bd3a24638ea8451cf0ae311b
Admin <Admin@TEB123007>
2022-09-09 09:13:36 +0200

    Add project files.

---
+++ kap 1-3 uppgift 5/kap_1_3_uppgift_5.py
@@ -0,0 +1,12 @@
+
+tankad=float(input("Hur mycket liter har du tankat? "))
+pris=float(input("Hur mycket kostar en liter bensin? "))
+totalt=pris * tankad
+print("+ - - - - - - - - - - - - - - - - +")
+print("-              KVITTO             -")
+print("-     Tankad volym:", tankad, "liter    -")
+print("- Pris pa en liter bensin", pris, "kr -")
+print("-         Betalt", totalt, "kr        -")
+print("-        Tack for besok och       -")
+print("-        valkommen ater!          -")
+print("+ - - - - - - - - - - - - - - - - +")
